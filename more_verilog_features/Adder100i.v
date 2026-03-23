20e25712da484e8017087b21bf53634030d91692
vnaeumann <venkatnarayanants@gmail.com>
2026-03-23 16:15:43 +0530

    Create Adder100i.v

---
+++ more_verilog_features/Adder100i.v
@@ -0,0 +1,24 @@
+module top_module( 
+    input [99:0] a, b,
+    input cin,
+    output [99:0] cout,
+    output [99:0] sum );
+    
+	assign sum[0] = a[0]^b[0]^cin;
+    assign cout[0]=a[0]&b[0] | a[0]&cin | b[0]&cin;
+    
+    always @(*) begin
+        for(int i=1; i< $bits(a); i++) begin
+             sum[i] = a[i]^b[i]^cout[i-1];
+             cout[i] = a[i]&b[i] | a[i]&cout[i-1] | b[i]&cout[i-1];
+        end
+    end
+    
+            
+    
+    
+    
+    
+    
+
+endmodule
